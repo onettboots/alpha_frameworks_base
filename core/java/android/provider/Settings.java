29d27f9e88fe4ce854d260f048f709b4fb55ce24
StarkDroid <gamerprince.exp@gmail.com>
2022-06-18 19:53:30 -0500

    Squashed QSPanel style and design changes
    QSPanel: Design changes
    
    * Switch to rounded rectangular tiles
    * Slim designed brightness bar
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Add QS Panel Styles [1/3]
    
    Note for other roms:
    This is the jankiest possible implementation that DOES WORK but is more of an extra work.
    Ideally i'd like to have this implemented within in-line ThemeOverLayApplier and ThemeOverlayController but rather
    its being handled through settings with a custom observer at the moment to trigger the overlays.
    
    So why this impl?
    I was just curious to test this out, but once i saw that its implemented and is working, i decided to push it and work on the refinement later
    
    Feel free to do the refinement on your end if you are able to :D
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Partial Revert "QSPanel: Design changes"
    
    Prepping this for QS Panel Styles
    
    This reverts commit ef0c7cabca565e8ff4b2f24d5023c8ce9d51f030.
    
    QSTile: More slower & Gradual QS Fade-in/out animation
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Tile: Set ripple color to accent by default
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Tile: Make QS Ripple animation more prominent
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Panel: Add Two-Tone Accented QS Style [1/3]
    
    Inspired by Fluid-UI's QS Panel changes
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    QS Styles: Add QS tiles color pop [1/2]
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    QSPanel Styles: Add key into backup and validator
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    QS Style: Make Outline QS Style truly outline
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    mnri: forward port to QPR1, squashed modifications
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6391,6 +6391,11 @@ public final class Settings {
         public static final String AMBIENT_CUSTOM_IMAGE = "ambient_custom_image";
 
         /**
+         * @hide
+         */
+        public static final String QS_PANEL_STYLE = "qs_panel_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
