5efabd5a79c4cc6861e798fef011ff3f3f147343
yunseon.park <yunseon.park@lge.com>
2022-07-14 11:00:40 +0900

    Fix bugs a task skipped setForceHidden and remained RESUMED state when setHidden of TaskDisplayArea
    - scenario
    1. Device has multiple TDAs and secondary TDA has more than 2 tasks.
    2. We can fill the large or extra screen space using multiple TDA's hidden or not.
    
    - problem
    1. There are 2 tasks testApp(#1) and launcher(#0) in secondary TDA.
    2. Launcher(#0) activity remains FOCUSED, TOP and RESUMED when setHidden of secondary TDA.
    3. Task.setForceHidden has designed for not DisplayArea but single task as PIP.
    4. After 1st topToBottom loop of forAllTasks, the list changed by #1's moveToBack
    and the result #0 skipped setForceHidden.
    5. So setForceHidden's moveToBack parameter set true only for the single task.
    
    Change-Id: If536f2751497452a5718cef48604769ce33720d2
    Signed-off-by: Yunseon Park <yunseon.park@lge.com>

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -4404,6 +4404,10 @@ class Task extends TaskFragment {
      * @return Whether the force hidden state changed
      */
     boolean setForceHidden(int flags, boolean set) {
+        return setForceHidden(flags, set, true /*moveToBack*/);
+    }
+
+    boolean setForceHidden(int flags, boolean set, boolean moveToBack) {
         int newFlags = mForceHiddenFlags;
         if (set) {
             newFlags |= flags;
@@ -4420,7 +4424,7 @@ class Task extends TaskFragment {
         final boolean nowHidden = isForceHidden();
         if (wasHidden != nowHidden) {
             final String reason = "setForceHidden";
-            if (wasVisible && nowHidden) {
+            if (wasVisible && nowHidden && moveToBack) {
                 // Move this visible task to back when the task is forced hidden
                 moveToBack(reason, null);
             } else if (isAlwaysOnTop()) {
