de62abe7e3aa5b254facc7ca7f19d0586dda67ca
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-22 16:02:40 +0800

    SystemUI: Statusbar clock background chip styles [1/2]
    * reconfigure rounded corners
    * increase left/right padding
    * fix textview alignment
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -148,7 +148,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private final DumpManager mDumpManager;
     private final TunerService mTunerService;
     private ClockController mClockController;
-    private boolean mShowSBClockBg;
+    private int mShowSBClockBg;
     private StatusIconContainer mStatusIcons;
     private int mSignalClusterEndPadding = 0;
 
@@ -385,8 +385,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 updateBlockedIcons();
                 break;
             case STATUSBAR_CLOCK_CHIP:
-                mShowSBClockBg =
-                        TunerService.parseIntegerSwitch(newValue, false);
+                mShowSBClockBg = 
+                        TunerService.parseInteger(newValue, 0);
                 updateStatusBarClock();
                 break;
             case STATUS_BAR_BATTERY_STYLE:
@@ -402,13 +402,18 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     }
 
     private void updateStatusBarClock() {
-        if (mShowSBClockBg) {
-            mClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mClockView.setPadding(10,2,10,2);
-            mCenterClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mCenterClockView.setPadding(10,2,10,2);
-            mRightClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mRightClockView.setPadding(10,2,10,2);
+        if (mShowSBClockBg != 0) {
+            String chipStyleUri = "sb_date_bg" + String.valueOf(mShowSBClockBg);
+            int resId = getContext().getResources().getIdentifier(chipStyleUri, "drawable", "com.android.systemui");
+            mClockView.setBackgroundResource(resId);
+            mClockView.setPadding(12,4,12,4);
+            mClockView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            mCenterClockView.setBackgroundResource(resId);
+            mCenterClockView.setPadding(12,4,12,4);
+            mCenterClockView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            mRightClockView.setBackgroundResource(resId);
+            mRightClockView.setPadding(12,4,12,4);
+            mRightClockView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         } else {
             int clockPaddingStart = getResources().getDimensionPixelSize(
                     R.dimen.status_bar_clock_starting_padding);
@@ -424,6 +429,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             mCenterClockView.setPaddingRelative(0,0,0,0);
             mRightClockView.setBackgroundResource(0);
             mRightClockView.setPaddingRelative(clockPaddingStart, 0, clockPaddingEnd, 0);
+            mClockView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+            mCenterClockView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+            mRightClockView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         }
     }
 
