02265ce1be3a40931aef36ec06f0acbec32b43d4
onettboots <blackcocopet@gmail.com>
2023-05-05 04:47:54 +0000

    change speed window animation scale
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1000,9 +1000,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    private float mWindowAnimationScaleSetting = 0.5f;
-    private float mTransitionAnimationScaleSetting = 0.5f;
-    private float mAnimatorDurationScaleSetting = 0.5f;
+    private float mWindowAnimationScaleSetting = 0.9f;
+    private float mTransitionAnimationScaleSetting = 0.9f;
+    private float mAnimatorDurationScaleSetting = 0.9f;
     private boolean mAnimationsDisabled = false;
     boolean mPointerLocationEnabled = false;
 
